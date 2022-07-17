105bba3b84edb517c8126a7efadca474be2c13f6
RediG7 <guleksiredi7@gmail.com>
2022-07-17 21:36:14 +0200

    User Auth Endpoint & JWT Initial Generation

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,22 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+// Accept JSON data in the body (req.body)
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
