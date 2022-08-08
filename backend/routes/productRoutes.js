81541a7bfc8433ee251438a0d32433a7ba1c1cc0
RediG7 <guleksiredi7@gmail.com>
2022-08-08 21:15:43 +0200

    Admin Delete Products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 // Like this
 router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 // Or like this:
 // router.get("/", getProducts)
 // router.get("/:id", getProductById);
--- frontend/src/screens/ProductListScreen.jsx
+++ frontend/src/screens/ProductListScreen.jsx
@@ -5,7 +5,7 @@ import { Table, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { listProducts } from "../actions/productActions";
+import { listProducts, deleteProduct } from "../actions/productActions";
 
 const ProductListScreen = () => {
   const dispatch = useDispatch();
@@ -14,6 +14,13 @@ const ProductListScreen = () => {
 
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
+
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = productDelete;
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
@@ -24,7 +31,7 @@ const ProductListScreen = () => {
     } else {
       navigate("../login");
     }
-  }, [dispatch, navigate, userInfo]);
+  }, [dispatch, navigate, userInfo, successDelete]);
 
   const createProductHandler = (product) => {
     // CREATE PRODUCT
@@ -32,7 +39,7 @@ const ProductListScreen = () => {
 
   const deleteHandler = (id) => {
     if (window.confirm("Are your sure?")) {
-      // DELETE PRODUCTS
+      dispatch(deleteProduct(id));
     }
   };
 
@@ -48,6 +55,8 @@ const ProductListScreen = () => {
           </Button>
         </Col>
       </Row>
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant="danger">{errorDelete}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
