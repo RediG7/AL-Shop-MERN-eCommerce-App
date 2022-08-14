e68d9ac7324b7f0e37ce5107f47f6a679bfab17a
RediG7 <guleksiredi7@gmail.com>
2022-08-14 21:15:59 +0200

    Image Upload Config & Endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -6,6 +7,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -22,8 +24,16 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+// Making uploads folder static so it is accessible on the browser
+// Because __dirname is only available with CommonJS, but we're using Modules so we do path.resolve() which does the same (point to the current directory).
+const __dirname = path.resolve;
+app.use("/uploads", express.static(path.join(__dirname)));
 
 app.use(notFound);
 
